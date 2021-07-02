ab8a08835e71812277d8189e20d99cb0b5167bb4
Tom Smith <ThomUK@users.noreply.github.com>
2021-07-02 08:49:39 +0100

    add the legend to the bottom of the plot

--- R/createGgplot.R
+++ R/createGgplot.R
@@ -87,6 +87,79 @@ createGgplot <- function(df, facetField, plotOptions){
   plot <- plot +
     plotOptions$themeOverride
 
-  return(plot)
+  #create the grid viewports
+  key_vp <- grid::viewport(x = 0, y = 0,
+                        width = 1, height = 0.1,
+                        just = c("left", "bottom"))
 
+  spc_vp <- grid::viewport(x = 0, y = 1,
+                            width = 1, height = 0.9,
+                            just = c("left", "top"))
+
+  #lay out the viewports and add a border
+  grid::grid.newpage()
+  grid::grid.draw(grid::rectGrob(gp = grid::gpar(col = "grey", lwd = 2))) #outer grey border
+  grid::pushViewport(key_vp)
+  grid::popViewport()
+  grid::pushViewport(spc_vp)
+  grid::grid.draw(ggplotGrob(plot))
+  grid::popViewport()
+
+  #draw the grobs into the key viewport
+  .drawLine(key_vp, 0.1, 0.06, "black" , 1, 2)
+  .drawTxt(key_vp, "Mean", 0.1)
+
+  .drawDot(key_vp, 0.26, .darkgrey)
+  .drawLine(key_vp, 0.26, 0.06, .darkgrey)
+  .drawTxt(key_vp, "Data", 0.26)
+
+  .drawLine(key_vp, 0.42, 0.06, .darkgrey, "33")
+  .drawTxt(key_vp, "Process limits \n(3 sigma)", 0.42)
+
+  .drawDot(key_vp, 0.58, .orange)
+  .drawTxt(key_vp, "Special cause \n(concern)", 0.58)
+
+  .drawDot(key_vp, 0.74, .skyblue)
+  .drawTxt(key_vp, "Special cause \n(improvement)", 0.74)
+
+  .drawLine(key_vp, 0.9, 0.06, .red, "33")
+  .drawTxt(key_vp, "Target", 0.9)
+
+}
+
+#Helper function for drawing consistent dots in the key
+.drawDot <- function(destination_vp, x, colour){
+  grid::grid.circle(
+    x = x,
+    y = 0.95,
+    r = 0.1,
+    gp = grid::gpar(fill = colour, col = colour),
+    vp = destination_vp
+  )
+}
+
+#Helper function for drawing consistent lines in the key
+.drawLine <- function(destination_vp, xPos, length, colour, linetype = 1, lineweight = 4){
+  l2 <- length / 2
+  grid::grid.lines(
+    x = c(xPos - l2, xPos + l2),
+    y = c(0.95, 0.95),
+    gp = grid::gpar(
+      col = colour,
+      lwd =lineweight,
+      lty = linetype
+    ),
+    vp = destination_vp
+  )
+}
+
+#Helper function for drawing consistent text in the key
+.drawTxt <- function(destination_vp, text, xPos){
+  grid::grid.text(
+    label = text,
+    x = xPos,
+    y = 0.5,
+    gp = grid::gpar(fontsize = 10),
+    vp = destination_vp
+  )
 }
